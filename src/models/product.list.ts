65a432f842a8a8d7442d399bf2357a7146c6edb3
Lalit.Kushwah <lalitkushwah52525@gmail.com>
2018-01-18 11:41:12 +0530

    Added: Models for inventory receive application

---
+++ src/models/product.list.ts
@@ -0,0 +1,13 @@
+export interface ProductList {
+
+  cat: string;
+  completeUrl: string;
+  facetDetails: Object;
+  feature: string;
+  listSize: string;
+  productList: Object;
+  viewIndex: number;
+  viewLastIndex: number;
+  viewSize: number;
+
+}
