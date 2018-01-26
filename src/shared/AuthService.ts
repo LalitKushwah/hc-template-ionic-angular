94126185e9a1f5ad93ca6c9f75aae6326b3368be
Anil Patel <anil.patel@hotwaxsystems.com>
2018-01-26 15:05:55 +0000

    Update AuthService.ts

--- src/shared/AuthService.ts
+++ src/shared/AuthService.ts
@@ -2,8 +2,11 @@ import { HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable()
+
+// Name suggests that the service is responsible for authorization. The methods on it does not reflect the expected behavior.
 export class AuthService {
 
+// We should not follow the cached request pattern.
   cachedRequest: HttpRequest<any> = null;
   targetScreen: string= null;
 
