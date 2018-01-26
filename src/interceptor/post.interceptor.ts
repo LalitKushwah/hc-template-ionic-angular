bd1853860bfa06b155d82cc4d5136a85e437e8d1
Anil Patel <anil.patel@hotwaxsystems.com>
2018-01-26 21:47:09 +0000

    Update post.interceptor.ts

--- src/interceptor/post.interceptor.ts
+++ src/interceptor/post.interceptor.ts
@@ -6,18 +6,28 @@ import {App} from "ionic-angular";
 import {AuthService} from "../shared/AuthService";
 import {LoginPage} from "../pages/login/login";
 
+// We should rename this interceptor to AuthIntercepor 
 @Injectable()
 export class PostInterceptor implements HttpInterceptor {
 
+// Http interceptor is powerful object, the API is concise so read every word with attention.
+// https://angular.io/api/common/http/HttpInterceptor
+
+  // Is this constructor empty for a reason or is it just WIP?
   constructor(public app: App,public authService:AuthService){};
 
+  // This is where the fun happens :)
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let cloneRequest =null;
-      let authenticationToken = localStorage.getItem('token');
+    
+    // Is local storage injected in this interceptor or is it a global variable.
+    // We should be careful about using global variables.
+    let authenticationToken = localStorage.getItem('token');
 
-   // If we are going to clone the req anyways, why not keep it out of the following condition statement and clone it as we initialize the variable.
+    // If we are going to clone the req anyways, why not keep it out of the following condition statement and clone it as we initialize the variable.
     if(authenticationToken != null) {
       cloneRequest = req.clone({
+        // I don't understand following line. Are we seeting the token in orignal req object ? asking because you told me we should not edit req object.  
         headers: req.headers.set('Bearer',authenticationToken).append('Content-Type', 'application/json')
       });
     }
@@ -26,23 +36,21 @@ export class PostInterceptor implements HttpInterceptor {
     }
 
     return next.handle(cloneRequest).do((event: HttpEvent<any>) => {
-      if (event instanceof HttpResponse) {
-        // do stuff with response if you want
-      }
-    }, (err: any) => {
-      if (err instanceof HttpErrorResponse) {
-        if (err.status === 401) {
-          // Remove invalid token
-          localStorage.removeItem('token');
-
-          //Store failed request and component name
-          this.authService.collectFailedRequest(cloneRequest);
-          this.authService.targetScreen = this.app.getActiveNavs().pop().getActive().name;
-
-          //Redirect user to login screen
-          this.app.getActiveNav().push(LoginPage);
+        if (event instanceof HttpResponse) {
+          // do stuff with response if you want
         }
-      }
-    });
+      }, (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            // Remove invalid token
+            localStorage.removeItem('token');
+            //Store failed request and component name
+            this.authService.collectFailedRequest(cloneRequest);
+            this.authService.targetScreen = this.app.getActiveNavs().pop().getActive().name;
+            //Redirect user to login screen
+            this.app.getActiveNav().push(LoginPage);
+          }
+        }
+      });
   }
 }
